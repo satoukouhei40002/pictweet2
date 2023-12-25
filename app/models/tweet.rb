bf5c56d81475e3fca8d339c7e6b897a6b8bbb578
satoukouhei40002 <a.k.o.o.z.vv@icloud.com>
2023-12-25 11:04:02 +0900

    Implementation of editing function

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,5 @@
 class Tweet < ApplicationRecord
+  validates :text, presence: true
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+<<<<<<< Updated upstream
   # Defines the root path route ("/")
   # root "articles#index"
+=======
+  root to: 'tweets#index'
+  resources :tweets, only: [:index, :new, :create, :destroy, :edit]
+>>>>>>> Stashed changes
 end
