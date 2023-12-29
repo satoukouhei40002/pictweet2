227a93a5a1ad28b109116ae4d6865628e33efcf9
satoukouhei40002 <a.k.o.o.z.vv@icloud.com>
2023-12-29 13:51:07 +0900

    詳細ページなどなど

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_15_121653) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_28_093451) do
+  create_table "tweets", charset: "utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "text"
+    t.text "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -19,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_15_121653) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
